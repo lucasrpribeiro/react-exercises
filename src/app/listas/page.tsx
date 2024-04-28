243949421135016215f88b12a4862ce040ed0384
Lucas <lucas.ribeiro711@gmail.com>
2024-04-28 12:33:57 -0300

    exercise: rendering list 1

--- src/app/listas/page.tsx
+++ src/app/listas/page.tsx
@@ -1,10 +1,20 @@
 // Renderizar listas no html
 
+function createList (length: number) {
+  const list = []
+
+  for (let i = 0; i <= length; i++) {
+    list.push(<li>{i}</li>)
+  }
+
+  return list
+}
+
 
 export default function Lista () {
   return (
     <ul>
-      <li>algo</li>
+      { createList(10) }
     </ul>
   )
 }
